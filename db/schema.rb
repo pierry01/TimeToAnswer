1b207227281fac7ca0b5081ef0c465c2abf85e07
pierry01 <pierrybm@gmail.com>
2019-06-06 00:37:32 -0300

    counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_144449) do
+ActiveRecord::Schema.define(version: 2019_06_06_032902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_144449) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
